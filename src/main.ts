849dca280efd1d476fa167bce809446cffe25f78
Hugo Averty <hugoaverty@gmail.com>
2022-08-08 15:57:54 +0200

    Adding overlay

--- src/main.ts
+++ src/main.ts
@@ -13,12 +13,16 @@ WA.onInit().then(() => {
         WA.room.hideLayer("roofOffice0");
         WA.room.hideLayer("roofOffice1");
         WA.room.hideLayer("roofOffice2");
+
+        WA.room.showLayer("roofOfficeOverlay");
     })      
 
     WA.room.onLeaveLayer('officeZone').subscribe(() => {
         WA.room.showLayer("roofOffice0");
         WA.room.showLayer("roofOffice1");
         WA.room.showLayer("roofOffice2");
+
+        WA.room.hideLayer("roofOfficeOverlay");
     })
 
 
@@ -26,12 +30,16 @@ WA.onInit().then(() => {
         WA.room.hideLayer("roofNFT0");
         WA.room.hideLayer("roofNFT1");
         WA.room.hideLayer("roofNFT2");
+
+        WA.room.showLayer("roofNFTOverlay");
     })      
 
     WA.room.onLeaveLayer('nftZone').subscribe(() => {
         WA.room.showLayer("roofNFT0");
         WA.room.showLayer("roofNFT1");
         WA.room.showLayer("roofNFT2");
+
+        WA.room.hideLayer("roofNFTOverlay");
     })
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
